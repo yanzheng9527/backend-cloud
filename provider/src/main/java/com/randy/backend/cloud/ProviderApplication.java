cb5e6deb3944768d27e59b2d2afd2fa87ee37551
randy0098 <lyon0098@163.com>
2020-09-28 17:26:24 +0800

    service provider test.

--- provider/src/main/java/com/randy/backend/cloud/ProviderApplication.java
+++ provider/src/main/java/com/randy/backend/cloud/ProviderApplication.java
@@ -2,12 +2,14 @@ package com.randy.backend.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+// 支持服务发现。
+@EnableDiscoveryClient
 public class ProviderApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProviderApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(ProviderApplication.class, args);
+  }
 }
