d7dafd52dc9222edc578a38928815e853c7b9270
randy0098 <lyon0098@163.com>
2020-10-12 16:28:10 +0800

    base-gateway init.

---
+++ base-gateway/src/main/java/com/randy/backend/cloud/BaseGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.randy.backend.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaseGatewayApplication.class, args);
+	}
+
+}
