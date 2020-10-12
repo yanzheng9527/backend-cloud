c274b1114e67614c4c6c9deafd7c9d34acfe2973
randy0098 <lyon0098@163.com>
2020-10-12 16:43:49 +0800

    base-gateway与consul集成，自动代理consul上的服务。

--- base-gateway/src/main/java/com/randy/backend/cloud/BaseGatewayApplication.java
+++ base-gateway/src/main/java/com/randy/backend/cloud/BaseGatewayApplication.java
@@ -2,12 +2,14 @@ package com.randy.backend.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+// 支持服务发现。
+@EnableDiscoveryClient
 public class BaseGatewayApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BaseGatewayApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(BaseGatewayApplication.class, args);
+  }
 }
